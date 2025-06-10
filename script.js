ef1f71cc81b903cd1bfffb73882280496cf601b7
Arlyss West <arlyss@pdx.edu>
2025-06-09 18:20:33 -0700

    added contact page

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 // CS-463 Intro To Web Development
 // Final Project: Personal Website
 // "script.js": shared java script
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("contactForm");
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      document.getElementById("formStatus").textContent =
+        "Thank you! Message sent.";
+      form.reset();
+    });
+  }
+});
